cf961127218d8abc1d508becd0af689420c9c5bd
Eric Sommerhalder <eric.sommerhalder@unibas.ch>
2020-06-03 22:35:39 +0200

    None

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   },
   {path: 'login', component : LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '**', component: HomeComponent}
+  {path: '**', component: LoginComponent}
 ];
 
 @NgModule({
