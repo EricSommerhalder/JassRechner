4454f3fe68341a9df519c544f8d3280ab88c7c53
Eric Sommerhalder <eric.sommerhalder@unibas.ch>
2020-10-13 20:50:32 +0200

    Routing after login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDejPfXCDXRQD7ctTdBW5E7-Qxmw6Fufc8',
+    authDomain: 'jassrechner.firebaseapp.com',
+    databaseURL: 'https://jassrechner.firebaseio.com',
+    projectId: 'jassrechner',
+    storageBucket: 'jassrechner.appspot.com',
+    messagingSenderId: '675676959816',
+    appId: '1:675676959816:web:83cf4df568dccabdb7d47d'
+  }
 };
